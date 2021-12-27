a86847d59cc3d1f7e5ccc588d14c1a5002e11dcb
jon4hz <admin@psytux.com>
2021-12-27 12:15:30 +0100

    readme

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Bermuda
-A docker garbage collector
+A simple docker garbage collector.
 
 _WIP_
+## About
+Bermuda is a simple wrapper around the docker prune commands to clean up old containers and images.
+
+## Usage
+TODO
--- cmd/version.go
+++ cmd/version.go
@@ -9,7 +9,7 @@ import (
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "Print the version number of bermuda",
+	Short: "Print the version info of bermuda",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Printf("Version: %s\n", version.Version)
 		fmt.Printf("Commit: %s\n", version.Commit)
